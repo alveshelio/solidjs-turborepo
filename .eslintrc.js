462d5c5a010130c660dbdff2e9be6a30c37d69e3
Helio Alves <helios25@gmail.com>
2022-08-05 22:30:39 +0100

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
+  extends: ["solidjs"],
   settings: {
-    next: {
+    solid: {
       rootDir: ["apps/*/"],
     },
   },
